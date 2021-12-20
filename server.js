e611d57c90990e8b747ea6a173b2b5c00673c8ad
IkoAfianando <ikoafianando123@gmail.com>
2021-12-20 09:25:22 +0700

    Push

--- server.js
+++ server.js
@@ -1,64 +1,44 @@
 const http = require("http");
 
 const requestListener = (request, response) => {
-  response.setHeader("content-type", "text/html");
-  response.statusCode = 200;
+    response.setHeader("content-Type", "application/json");
+    response.setHeader("X-Powered-By", "NodeJS");
 
-  const { method, url } = request;
+    const {method, url} = request;
 
-  if (url === "/") {
-    if (method === "GET") {
-      response.end(`<p>Ini adalah Homepage</p>`);
+    if (url === "/") {
+        if (method === "GET") {
+            response.statusCode = 200;
+            response.end(`<p>Ini adalah Homepage</p>`);
+        } else {
+            response.statusCode = 400;
+            response.end(`<p>Halaman tidak dapat diakses dengan menggunakan ${method} request</p>`);
+        }
+    } else if (url === "/about") {
+        if (method === "GET") {
+            response.statusCode = 200;
+            response.end(`<p>Halo! Ini adalah halaman about!</p>`);
+        } else if (method === "POST") {
+            let body = [];
+
+            request.on("data", (chunk) => {
+                body.push(chunk);
+            });
+
+            request.on("end", () => {
+                body = Buffer.concat(body).toString();
+                const {name} = JSON.parse(body);
+                response.statusCode = 200;
+                response.end(`<p>Hi! ${name} ini adalah halaman about!</p>`);
+            });
+        } else {
+            response.statusCode = 400;
+            response.end(`<p>Halamam tidak dapat diakses dengan menggunakan ${method} request</p>`);
+        }
     } else {
-      response.end(
-        `<p>Halam tidak dapat diakses dengan menggunakan ${method} request</p>`
-      );
+        response.statusCode = 404;
+        response.end(`<p>Halaman tidak ditemukan!</p>`);
     }
-  } else if (url === "/about") {
-    if (method === "GET") {
-      response.end(`<p>Halo! Ini adalah halaman about!</p>`);
-    } else if (method === "POST") {
-      let body = [];
-      request.on("data", (chunk) => {
-        body.push(chunk);
-      });
-
-      request.on("end", () => {
-        body = Buffer.concat(body).toString();
-        const { name } = JSON.parse(body);
-        response.end(`<p>Hi! ${name} ini adalah halaman about!</p>`);
-      });
-    } else {
-      response.end(
-        `<p>Halam tidak dapat diakses dengan menggunakan ${method} request</p>`
-      );
-    }
-  } else {
-    response.end(`<p>Halaman tidak ditemukan!</p>`);
-  }
-
-  // if (method === "GET") {
-  //   response.end("<h1>Hello!</h1>");
-  // }
-  // if (method === "POST") {
-  //   let body = [];
-  //   request.on("data", (chunk) => {
-  //     body.push(chunk);
-  //   });
-
-  //   request.on("end", () => {
-  //     body = Buffer.concat(body).toString();
-  //     const { name } = JSON.parse(body);
-  //     response.end(`<p>Hi! ${name}</p>`);
-  //   });
-  // }
-
-  // if (method === "PUT") {
-  //   response.end("<h1>Bonjour</h1>");
-  // }
-  // if (method === "DELETE") {
-  //   response.end("<h1>Salam</h1>");
-  // }
 };
 
 const server = http.createServer(requestListener);
@@ -67,5 +47,5 @@ const port = 5000;
 const host = "localhost";
 
 server.listen(port, host, () => {
-  console.log(`Server berjalan pada http://${host}:${port}`);
+    console.log(`Server berjalan pada http://${host}:${port}`);
 });
