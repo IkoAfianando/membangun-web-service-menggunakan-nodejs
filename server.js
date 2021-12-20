156f1cb11317a923414bc8f1a60e92820b786128
IkoAfianando <ikoafianando123@gmail.com>
2021-12-20 08:26:33 +0700

    Push

--- server.js
+++ server.js
@@ -4,7 +4,38 @@ const requestListener = (request, response) => {
   response.setHeader("content-type", "text/html");
   response.statusCode = 200;
 
-  const { method } = request;
+  const { method, url } = request;
+
+  if (url === "/") {
+    if (method === "GET") {
+      response.end(`<p>Ini adalah Homepage</p>`);
+    } else {
+      response.end(
+        `<p>Halam tidak dapat diakses dengan menggunakan ${method} request</p>`
+      );
+    }
+  } else if (url === "/about") {
+    if (method === "GET") {
+      response.end(`<p>Halo! Ini adalah halaman about!</p>`);
+    } else if (method === "POST") {
+      let body = [];
+      request.on("data", (chunk) => {
+        body.push(chunk);
+      });
+
+      request.on("end", () => {
+        body = Buffer.concat(body).toString();
+        const { name } = JSON.parse(body);
+        response.end(`<p>Hi! ${name} ini adalah halaman about!</p>`);
+      });
+    } else {
+      response.end(
+        `<p>Halam tidak dapat diakses dengan menggunakan ${method} request</p>`
+      );
+    }
+  } else {
+    response.end(`<p>Halaman tidak ditemukan!</p>`);
+  }
 
   // if (method === "GET") {
   //   response.end("<h1>Hello!</h1>");
